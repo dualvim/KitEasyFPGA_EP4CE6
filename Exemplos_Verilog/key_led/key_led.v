11a30812c525217f9e3c4ec5e278f9cb67ecb1cb
Diego Nagai <diego_nagai@terra.com.br>
2021-12-12 16:53:37 -0300

    Update key_led.v
    Alteração do cabeçalho do arquivo key_led.v

--- Exemplos_Verilog/key_led/key_led.v
+++ Exemplos_Verilog/key_led/key_led.v
@@ -1,13 +1,12 @@
-
+//*******************************************************
 // Projeto: Utilização dos leds e botões do kit
-//
 // Autor: Diego Nagai (https://github.com/diegonagai)
-//
 // Data: 06/dez/2021
+//*******************************************************
 
 module key_led (key, led);
 
-input [1:4] key;
+input  [1:4] key;
 output [1:4] led;
 
 assign led = key;
